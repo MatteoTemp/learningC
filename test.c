669f51ef69ab706e9b375ed095d00bb3458a904e
MatteoTemp <matteotempella20@gmail.com>
2024-06-05 10:54:56 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,16 @@
 
-#include "LinearAlgebra.h"
+#include "LinalgAlgorithms.h"
+#include "matrix.h"
 #include <math.h>
 
 int main(){
 
-    show(ROT(M_PI, M_PI, M_PI));
+    Matrix * m = init(3, 1);
+    m->elements[0][0] = 1.0;
+    m->elements[1][0] = 1.0;
 
+    show(m);
+
+    printf("%f\n", normp(m, 2));
     return 0;
 }
